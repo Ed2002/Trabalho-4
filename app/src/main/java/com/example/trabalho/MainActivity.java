0c0251fde902f30ff3bdbb77b8ea241be2c012ba
Ed2002 <ed49516@gmail.com>
2023-05-24 00:37:39 -0300

    Fragments

--- app/src/main/java/com/example/trabalho/MainActivity.java
+++ app/src/main/java/com/example/trabalho/MainActivity.java
@@ -1,14 +1,52 @@
 package com.example.trabalho;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.ktx.Firebase;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button BtnListar, BtnNovo;
+    private ListarFragment ListarFragment;
+    private NovoEditarFragment NovoEditarFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BtnListar = findViewById(R.id.BtnListar);
+        BtnNovo = findViewById(R.id.BtnNovo);
+        ListarFragment = new ListarFragment();
+        NovoEditarFragment = new NovoEditarFragment();
+
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.add(R.id.FrameConteudo, ListarFragment);
+        transaction.commit();
+
+        BtnNovo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.FrameConteudo, NovoEditarFragment);
+                transaction.commit();
+            }
+        });
+
+        BtnListar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.FrameConteudo, ListarFragment);
+                transaction.commit();
+            }
+        });
+
     }
 }
